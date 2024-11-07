89583bbc043c5ef94f5bcc1b96a965142cedab9a
12999355@itisrossi.vi.it <12999355@itisrossi.vi.it>
2024-11-07 18:40:08 +0100

    es1

--- es_if_3/script.js
+++ es_if_3/script.js
@@ -1,35 +1,94 @@
-function es(n){
-
-    if (n=`galleria`){
-
+function form_galleria(){
+//inizializzo il form dove andrò a scrivere i dati
         document.getElementById("form").innerHTML = `
         <select id="cascata">
         <option name="img" value="mare" id="mare">Mare</option>
         <option name="img" value="montagna">Montagna</option>
         <option name="img" value="città">Città</option>
         </select>
-       
+        <br>
         <br>
         <label for="nero">Bianco e nero</label>
         <input type="radio" name="bianconero" value="nero" id="nero">
         <label for="colori">A colori</label>
         <input type="radio" name="bianconero" value="colori" id="colori">
-
+        <br>
+        <br>
         <button onclick="es_immagini()">guarda</button>
-    `
-    } else{
+        <br>
 
-    }
+    `
+    document.querySelector("body").setAttribute("style", "background-color: #B5954A;");
 
 }
 
+function form_ristorante(){
+//inizializzo il form dove andrò a scrivere i dati
+    document.getElementById("form").innerHTML = `
+    <select id="cascata">
+    <option name="valutazione" value="1">&#11088</option>
+    <option name="valutazione" value="2">&#11088 &#11088</option>
+    <option name="valutazione" value="3">&#11088 &#11088 &#11088</option>
+    <option name="valutazione" value="4">&#11088 &#11088 &#11088 &#11088</option>
+    <option name="valutazione" value="5">&#11088 &#11088 &#11088 &#11088 &#11088</option>
+    </select>
+   <p>Recensione:</p>
+    <label for="positivo">Positiva</label>
+    <input type="radio" name="pos_neg" value="positivo" id="positivo">
+    <label for="negativo">Negativa</label>
+    <input type="radio" name="pos_neg" value="negativo" id="negativo">
+    <br>
+    <br>
+    <button onclick="es_ristorante()">lascia recensione</button>
+    <br>
+    <br>
+    <p id="risposta"></p>
+`
+document.querySelector("body").setAttribute("style", "background-color: #4855B7;");
+}
+
+
 function es_immagini(){
+//leggo in input i vari valori delle caselle di input
     let tipo = document.querySelector("input[name=bianconero]:checked").value;
     let img = document.getElementById("cascata").value;
     
+    document.getElementById("output").setAttribute("style", "height: 250px; witdh: 35px");
+// controllo tutti i vari casi che si possono combinare con i dati e fornisco in output l'adeguata immagine
+    switch (img){
 
-    alert(tipo)
-    alert(img)
+        case "mare":        
+            if(tipo=="colori"){
+                document.getElementById("output").setAttribute("src", "img/mare_1.jpg");
+            } else{
+                document.getElementById("output").setAttribute("src", "img/mare_1_grigio.jpg");
+            }
+            break;
+        case "montagna": 
+        if(tipo=="colori"){
+            document.getElementById("output").setAttribute("src", "img/montagna.jpg");
+        } else{
+            document.getElementById("output").setAttribute("src", "img/montagna_grigio.jpg");
+        }
+            break;
+        case "città":
+            if(tipo=="colori"){
+                document.getElementById("output").setAttribute("src", "img/città.jpg");
+            } else{
+                document.getElementById("output").setAttribute("src", "img/città_grigio.jpg");
+            }
+            break;
+        default:
+            alert("errore in input")
+            break;
+
+
+    }
     
     }
+
+    function es_ristorante(){
+    
+        document.getElementById("risposta").innerText=`ciao`
+        }
 
