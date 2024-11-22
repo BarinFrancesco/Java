b67793bb62bb504092e2afde7ade4525c57ec00c
12999355@itisrossi.vi.it <12999355@itisrossi.vi.it>
2024-11-22 22:03:32 +0100

    fine feedback

--- es_21_11_2024/feedback/script.js
+++ es_21_11_2024/feedback/script.js
@@ -11,88 +11,100 @@ function analyzeFeedback(){
     let count_u = 0;
     let word_count = 1;
 
-    for (let i = 0; i < feedback.length; i++) {
+    if(feedback!=``){
+        for (let i = 0; i < feedback.length; i++) {
 
-        let caracter = feedback[i];
-        let code = caracter.charCodeAt(0);
+            let caracter = feedback[i];
+            let code = caracter.charCodeAt(0);
 
-        const vowels = /[aAeEiIoOuU]/gi;
+            const vowels = /[aAeEiIoOuU]/gi;
 
-        if ((code >= 65 && code <= 90) || (code >= 97 && code <= 122)){
+            if ((code >= 65 && code <= 90) || (code >= 97 && code <= 122)){
 
-            if(caracter.match(vowels)){
+                if(caracter.match(vowels)){
 
-                switch (caracter){
+                    switch (caracter){
 
-                    case "a":
-                        count_a+=1;
-                    break;
+                        case "a":
+                            count_a+=1;
+                        break;
 
-                    case "A":
-                        count_a+=1;
-                    break;
-
-
-                    case "e":
-                        count_e+=1;
-                    break;
-
-                    case "E":
-                        count_e+=1;
-                    break;
+                        case "A":
+                            count_a+=1;
+                        break;
 
 
-                    case "i":
-                        count_i+=1;
-                    break;
+                        case "e":
+                            count_e+=1;
+                        break;
 
-                    case "I":
-                        count_i+=1;
-                    break;
-
-
-                    case "o":
-                        count_o+=1;
-                    break;
-
-                    case "O":
-                        count_o+=1;
-                    break;
+                        case "E":
+                            count_e+=1;
+                        break;
 
 
-                    case "u":
-                        count_u+=1;
-                    break;
+                        case "i":
+                            count_i+=1;
+                        break;
 
-                    default:
-                        count_u+=1;
-                    break;
+                        case "I":
+                            count_i+=1;
+                        break;
+
+
+                        case "o":
+                            count_o+=1;
+                        break;
+
+                        case "O":
+                            count_o+=1;
+                        break;
+
+
+                        case "u":
+                            count_u+=1;
+                        break;
+
+                        default:
+                            count_u+=1;
+                        break;
+                    }
+
+                    vowels_cont+=1;
+                } else {
+                    consonant_count+=1;
                 }
 
-                vowels_cont+=1;
-            } else {
-                consonant_count+=1;
+                length+=1;
+            } else if (code == 32){
+                word_count += 1; 
             }
+            
+        };
 
-            length+=1;
-        } else if (code == 32){
-            word_count += 1; 
+        let biggest_vowel = Math.max(count_a, count_e, count_i, count_o, count_u);
+        let risposta = `Vowels { A:${count_a}, E:${count_e}, I:${count_i}, O:${count_o}, U:${count_u}} <br> Consonants: ${consonant_count} <br> MostFrequentVowel: ${biggest_vowel} <br> Words: ${word_count}`;
+
+        if (length <= 10 ) {
+            risposta += `<br> FeedbackCategory: Scarso`
+        } else if(length < 50){
+            risposta += `<br> FeedbackCategory: Moderato`
+        } else{
+            risposta += `<br> FeedbackCategory: Dettagliato`
         }
-        
-    };
 
-    let biggest_vowel = Math.max(count_a, count_e, count_i, count_o, count_u);
-    let risposta = `vowels { A:${count_a}, E:${count_e}, I:${count_i}, O:${count_o}, U:${count_u}} <br> Consonants: ${consonant_count} <br> mostFrequentVowel: ${biggest_vowel} <br> word: ${word_count}`;
+        if (feedback.includes(`fantastico`) || feedback.includes(`eccellente`) || feedback.includes(`ottimo`)){
+            risposta += `<br> SatisfactionIndex: Positivo`
+        } else if (feedback.includes(`lento`) || feedback.includes(`pessimo`) || feedback.includes(`difettoso`)){
+            risposta += `<br> SatisfactionIndex: Negativo`
+        } else {
+            risposta += `<br> SatisfactionIndex: Neutro`
+        }
 
-    if (length <= 10 ) {
-        risposta += `<br> feedbackCategory: Scarso`
-    } else if(length < 50){
-        risposta += `<br> feedbackCategory: Moderato`
-    } else{
-        risposta += `<br> feedbackCategory: Dettagliato`
+        document.getElementById(`risposta`).innerHTML = `${risposta}`;
+    } else {
+        alert(`La tua recensione non esiste`);
     }
-
-    document.getElementById(`risposta`).innerHTML = `${risposta}`;
     /*let fantastico = feedback.match(/fantastico/i);
     let ottimo = feedback.match(/ottimo/i);
     let eccellente = feedback.match(/eccellente/i);
