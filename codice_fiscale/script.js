708ccb1abf56135f830a1fa7b88f50fb36d9ee00
12999355@itisrossi.vi.it <12999355@itisrossi.vi.it>
2024-12-03 21:37:47 +0100

    codice fiscale

--- codice_fiscale/script.js
+++ codice_fiscale/script.js
@@ -1,15 +1,27 @@
-let codice_fiscale
+let codice_fiscale =``;
+const vowels = /aeiou/;
 
-function user_name(){
-    let nome = document.getElementById(`nome`).value;
+function codice(){
+    
+    let cognome = user_name(`cognome`);
+    let nome = user_name(`nome`);
+
+
+    codice_fiscale = `${cognome}${nome}`;
+    alert(codice_fiscale);
+}
+
+function user_name(n){
+
+    let nome = document.getElementById(`${n}`).value;
     let name_caracther = ``;
     let conta = 0;
 
-    name = name.toLowercase();
+    nome = nome.toLowerCase();
 
     for (let i = 0; i<nome.length; i++){
 
-        if( !(nome[i]==="a" || nome[i]==="e" || nome[i]==="i" || nome[i]==="o" || nome[i]==="u") && conta<3 ){
+        if( !(nome[i]==="a" || nome[i]==="e" || nome[i]==="i" || nome[i]==="o" || nome[i]==="u") && conta<3) {   /* .include("aeiuo")  */
             name_caracther += nome[i];
             conta += 1;
         }
@@ -24,10 +36,11 @@ function user_name(){
             }
         }
 
+        if(conta<3){
+            name_caracther +=`x`
+        }
+
     }
-    codice_fiscale += name_caracther;
-}
 
-function user_surname(){
-
+    return name_caracther;
 }
