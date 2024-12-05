12d94ec517bcca74be51b0c3bfb842248450f9f2
BARIN FRANCESCO <12999355@itisrossi.edu.it>
2024-12-05 10:31:00 +0100

    codice fiscale

--- codice_fiscale/script.js
+++ codice_fiscale/script.js
@@ -5,6 +5,7 @@ function codice(){
     
     let cognome = user_name(`cognome`);
     let nome = user_name(`nome`);
+    let anno = data_nascita();
 
 
     codice_fiscale = `${cognome}${nome}`;
@@ -21,7 +22,7 @@ function user_name(n){
 
     for (let i = 0; i<nome.length; i++){
 
-        if( !(nome[i]==="a" || nome[i]==="e" || nome[i]==="i" || nome[i]==="o" || nome[i]==="u") && conta<3) {   /* .include("aeiuo")  */
+        if( !(nome[i]==="a" || nome[i]==="e" || nome[i]==="i" || nome[i]==="o" || nome[i]==="u") && conta<3) {   
             name_caracther += nome[i];
             conta += 1;
         }
@@ -43,4 +44,79 @@ function user_name(n){
     }
 
     return name_caracther;
+}
+
+function data_nascita(){
+    let data = document.getElementById("anno").value;
+    let data_code=``;
+    data = data.split("/");
+
+
+    if(data[0]>31 || data[0]<0){
+        alert("errore nel giorno della data");
+        return;
+    } 
+
+    if (data[1]>12 || data[1]<0){
+        alert("errore nel mese della data");
+
+        return;
+    } 
+
+    if (data[2]>2024 || data[2]<1900){
+        alert("errore nell'anno della data");
+
+        return;
+    }
+
+    data_code += `${data[2]}${data[3]}`
+
+    switch (data[1]){
+        case 1:
+            break;
+
+        case 2:
+
+         break;
+        case 3:
+            break;
+
+
+        case 4:
+            break;
+
+
+        case 5:
+            break;
+
+
+        case 6:
+            break;
+
+
+        case 7:
+             break;
+
+
+        case 8:
+            break;
+
+
+        case 9:
+                                        
+            break;
+
+
+        case 10:
+            break;
+
+
+        case 11:
+         break;
+
+
+        case 12:
+         break;
+
+    }
 }
