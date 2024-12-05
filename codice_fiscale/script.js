37e86d1968ff4c9dc06a891dcf6c386f8e028c57
12999355@itisrossi.vi.it <12999355@itisrossi.vi.it>
2024-12-05 19:54:32 +0100

    codice fiscale

--- codice_fiscale/script.js
+++ codice_fiscale/script.js
@@ -1,5 +1,5 @@
 let codice_fiscale =``;
-const vowels = /aeiou/;
+const vowels = "aeiou";
 
 function codice(){
     
@@ -8,7 +8,7 @@ function codice(){
     let anno = data_nascita();
 
 
-    codice_fiscale = `${cognome}${nome}`;
+    codice_fiscale = `${cognome}${nome}${anno}`;
     alert(codice_fiscale);
 }
 
@@ -22,23 +22,24 @@ function user_name(n){
 
     for (let i = 0; i<nome.length; i++){
 
-        if( !(nome[i]==="a" || nome[i]==="e" || nome[i]==="i" || nome[i]==="o" || nome[i]==="u") && conta<3) {   
+        if(!(vowels.includes(nome[i])) && conta<3) {   //==="a" || nome[i]==="e" || nome[i]==="i" || nome[i]==="o" || nome[i]==="u"
             name_caracther += nome[i];
             conta += 1;
         }
     }
 
     if(conta<3){
+
         for (let i = 0; i<nome.length; i++){
 
-            if ((nome[i]==="a" || nome[i]==="e" || nome[i]==="i" || nome[i]==="o" || nome[i]==="u") && conta<3){
+            if ((vowels.includes(nome[i])) && conta<3){
                 name_caracther += nome[i];
                 conta += 1;
             }
         }
 
         if(conta<3){
-            name_caracther +=`x`
+                name_caracther +=`x`
         }
 
     }
@@ -49,74 +50,82 @@ function user_name(n){
 function data_nascita(){
     let data = document.getElementById("anno").value;
     let data_code=``;
+    let mese;
+    let giorno;
+    let sesso = document.querySelector(`input[name='sesso']:checked`).value;
     data = data.split("/");
 
 
-    if(data[0]>31 || data[0]<0){
-        alert("errore nel giorno della data");
-        return;
-    } 
-
-    if (data[1]>12 || data[1]<0){
-        alert("errore nel mese della data");
-
-        return;
-    } 
-
-    if (data[2]>2024 || data[2]<1900){
-        alert("errore nell'anno della data");
-
+    if(data[0]>31 || data[0]<0 || data[1]>12 || data[1]<0 || data[2]>2024 || data[2]<1900){
+        alert("C'è unerrore nella data");
         return;
     }
 
-    data_code += `${data[2]}${data[3]}`
-
-    switch (data[1]){
+    switch (parseInt(data[1])){
         case 1:
+            mese = `a`;
             break;
 
         case 2:
-
-         break;
-        case 3:
+            mese = `b`;
             break;
 
+        case 3:
+            mese = `c`;
+            break;
 
         case 4:
+            mese = `d`;
             break;
 
 
         case 5:
+            mese = `e`;
             break;
 
 
         case 6:
+            mese = `h`;
             break;
 
 
         case 7:
+            mese = `l`;
              break;
 
 
         case 8:
+            mese = `m`;
             break;
 
 
         case 9:
-                                        
+            mese = `p`;                   
             break;
 
 
         case 10:
+            mese = `r`;
             break;
 
 
         case 11:
-         break;
+            mese = `s`;
+            break;
 
 
         case 12:
-         break;
-
+            mese = `t`;
+            break;
     }
+
+    if (sesso == `f`){
+        giorno = parseInt(data[0])+40;
+    } else {
+        giorno = data[0].toString().padStart(2, '0');
+    }
+
+    data_code += `${(data[2]%100).toString().padStart(2, '0')}${mese}${giorno}`;
+
+    return data_code;
 }
