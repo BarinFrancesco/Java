dc444539014e33509e3800a44dcd1f6faaca78b8
BARIN FRANCESCO <12999355@itisrossi.edu.it>
2024-12-14 09:36:39 +0100

    mastermind

---
+++ mastermind/script.js
@@ -0,0 +1,51 @@
+
+let row = 1;
+
+function change_color(x,y){
+    let btn = document.getElementById(`${row}-${y}`);
+    let color;
+
+    switch (btn.classList.toString()){
+        case "red":
+            color ="green";
+            break;
+
+        case "green":
+            color ="blue";
+            break;
+        
+        case "blue":
+            color ="yellow";
+            break;
+        
+        case "yellow":
+            color ="purple";
+            break;
+        
+        case "purple":
+            color ="light_blue";
+            break;
+
+        case "light_blue":
+            color ="gray";
+            break;
+
+        case "gray":
+            color ="red";
+            break;
+    }
+
+    btn.setAttribute("class", `${color}`);
+}
+
+function verifica(){
+        
+    row++
+    for (let i = 1; i<5; i++){
+        document.getElementById(`${row}-${i}`).setAttribute(`onclick`,`change_color(${row},${i})`)
+    }
+    for (let i = 1; i<5; i++){
+        document.getElementById(`${row-1}-${i}`).setAttribute(`onclick`,``)
+    }
+
+}
